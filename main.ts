ee9240ff5cce2e1bf2ad674511e992c70db0e4d5
Hacker-C <1507559148@qq.com>
2023-03-23 23:26:59 +0800

    🐛 fix: 返回数据为空，则请求前一天数据

--- main.ts
+++ main.ts
@@ -51,7 +51,8 @@ function Ascii2Utf8(str: string) {
 	return str.replace(/&#(\d+);/g, (_, $1) => String.fromCharCode(Number($1)));
 }
 
-const api = 'https://www.zhihu.com/api/v4/columns/c_1261258401923026944/items?limit=1';
+const api = (offset: number) =>
+	`https://www.zhihu.com/api/v4/columns/c_1261258401923026944/items?limit=1&offset=${offset}`;
 const oneHourMs = 60 * 60 * 1000;
 const cache: Record<number, string[]> = {};
 
@@ -62,8 +63,19 @@ async function handler(req: Request) {
 	const today = Math.floor((Date.now() + 8 * oneHourMs) / (24 * oneHourMs));
 
 	if (!cache[today]) {
-		const { data } = await (await fetch(api)).json();
-		const contents = data[0].content.match(
+		let offset = 0;
+		let content = null;
+		for (;;) {
+			const { data } = await (await fetch(api(offset))).json();
+			const res = data?.[0]?.content;
+      // BUGFIX: 若数据为空（专栏还未编写今日新闻），则往前一天请求，直到成功或尝试 10 次为止
+			if (res || offset > 10) {
+				content = res;
+				break;
+			}
+			offset++;
+		}
+		const contents = content.match(
 			/<p\s+data-pid=[^<>]+>([^<>]+)<\/p>/g,
 		);
 		cache[today] = contents.map((e: string) => Ascii2Utf8(e.replace(/<[^<>]+>/g, '')));
